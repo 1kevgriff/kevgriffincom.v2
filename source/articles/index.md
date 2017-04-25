da9b84e97cc8f9f17616e71b80e698b4acd6246c
Kevin Griffin <kevin@kevgriffin.com>
2017-04-25 14:20:45 -0400

    wip

---
+++ source/articles/index.md
@@ -0,0 +1,5 @@
+---
+title: articles
+layout: articles
+date: 2017-04-24 12:58:45
+---
--- themes/griffin/helper/index.js
+++ themes/griffin/helper/index.js
@@ -1,10 +1,32 @@
-module.exports = function(hexo) {
+var _ = require('lodash');
+
+module.exports = function (hexo) {
   return {
-    json: function(obj) {
-        console.log("-----");
-        console.log(obj);
-        console.log("#-----");
-      return "post";
+    recent_posts: function (count) {
+        options = {};
+
+      // default options
+      options = _.extend({
+        count: count,
+        ulClass: 'recent_posts',
+        liClass: 'recent_post'
+      }, options);
+
+      // get last posts
+      var posts = this.site
+        .posts.sort('date', -1)
+        .limit(options.count);
+
+      // build the list
+      var result = '<ul class="' + options.ulClass + '">';
+      var root = this.config.root;
+
+      posts.each(function (post) {
+        result += '<li class="' + options.liClass + '"><a href="' + root + post.path + '">' + post.title + '</a></li>';
+      });
+
+      result += '</ul>'
+      return result;
     }
   };
 };
