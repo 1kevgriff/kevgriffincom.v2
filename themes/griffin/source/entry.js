b99383acc6220565942ca47efa15f3b6ecc20fac
Kevin Griffin <kevin@kevgriffin.com>
2017-05-02 11:41:38 -0400

    wip

---
+++ themes/griffin/source/entry.js
@@ -0,0 +1,4 @@
+require("./plugins/jquery.min.js");
+import "./bootstrap/js/bootstrap.min.js"
+import "./plugins/modernizr.js"
+import "./plugins/rs-plugin-5/js/jquery.themepunch.tools.min.js?rev=5.0"
